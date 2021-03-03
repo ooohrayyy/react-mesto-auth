393f68274f36d9059c2e6a7c2adf97e503ba6e4e
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-03 18:44:30 +0300

    feat: Добавить метод _checkResponseData в модуль auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,4 +3,11 @@ class Auth {
     this._baseUrl = options.baseUrl;
     this._token = options.authorization;
   }
+
+  _checkResponseData (res) { // Проверка ответа от сервера
+    if (!res.ok) {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+    return res.json();
+  }
 }
