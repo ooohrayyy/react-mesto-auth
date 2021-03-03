e165f506054d8e1397c55426dc8c0e046ffec42f
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-03 14:07:02 +0300

    feat: Добавить логику в InfoPopup

--- src/components/InfoPopup.js
+++ src/components/InfoPopup.js
@@ -1,5 +1,11 @@
-function InfoPopup () {
-  return;
+import InfoToolTip from './InfoToolTip.js';
+
+function InfoPopup (props) {
+  return (
+    <div className={`popup popup-${props.name} root__popup ${props.state.open ? 'popup_opened' : ''}`}>
+      <InfoToolTip infoModifier={infoModifier} state={props.state} message={props.message} />
+    </div>
+  );
 }
 
 export default InfoPopup;
