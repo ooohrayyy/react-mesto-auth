392870956c84e6fd21286ae8c6e3ab1b5a96c941
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-03 12:00:10 +0300

    feat: Переписать Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import logo from '../resources/images/svgs/logo.svg';
 
 function Header (props) {
+  // * Переменные
+
+  const registerLink = (<a className="header__register" href="/sign-up">Регистрация</a>);
+  const authorizeLink = (<a className="header__register" href="/sign-in">Войти</a>);
+
+  // * Возвращаемое значение
+
   return (
     <header className="container__header header">
       <img className="header__logo" src={logo} alt="Логотип Mesto" />
-      {!props.loggedIn && (<a className="header__register" href="#">Регистрация</a>)}
+      {(props.place === 'login') && registerLink}
+      {(props.place === 'register') && authorizeLink}
     </header>
   );
 }
