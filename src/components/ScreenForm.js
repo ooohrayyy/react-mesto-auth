946d9da48f0cc1cd1199ea83184e2f1ec7771d36
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-03 12:10:05 +0300

    refactor: Переписать ссылку sign-in

--- src/components/ScreenForm.js
+++ src/components/ScreenForm.js
@@ -36,32 +36,34 @@ function ScreenForm (props) {
   // * Возвращаемое значение
 
   return (
-    <form className="form form_place_screen" noValidate onChange={handleChange}>
-      <h1 className="form__heading form__heading_place_screen">{headingText}</h1>
-      <Input
-        inputModifier="input_place_screen"
-        inputName="login_email"
-        inputValue={email}
-        inputType="email"
-        inputPlaceholder="Email"
-        inputValidityState={emailValidity}
-        onInputValidityChange={setEmailValidity}
-        onValueChange={handleEmailChange}
-      />
-      <Input inputType="password"
-        inputModifier="input_place_screen"
-        inputName="login_password"
-        inputValue={password}
-        inputType="password"
-        inputPlaceholder="Пароль"
-        inputMinLength="6"
-        inputValidityState={passwordValidity}
-        onInputValidityChange={setPasswordValidity}
-        onValueChange={handlePasswordChange}
-      />
-      <button className="form__button button button_place_screen" type="submit" disabled={!formValidity}>{buttonText}</button>
-      {(props.form === 'register') && (<a className="form__sign-in" href="/sign-in">Уже зарегистрировались? Войти</a>)}
-    </form>
+    <>
+      <form className="form form_place_screen" noValidate onChange={handleChange}>
+        <h1 className="form__heading form__heading_place_screen">{headingText}</h1>
+        <Input
+          inputModifier="input_place_screen"
+          inputName="login_email"
+          inputValue={email}
+          inputType="email"
+          inputPlaceholder="Email"
+          inputValidityState={emailValidity}
+          onInputValidityChange={setEmailValidity}
+          onValueChange={handleEmailChange}
+        />
+        <Input inputType="password"
+          inputModifier="input_place_screen"
+          inputName="login_password"
+          inputValue={password}
+          inputType="password"
+          inputPlaceholder="Пароль"
+          inputMinLength="6"
+          inputValidityState={passwordValidity}
+          onInputValidityChange={setPasswordValidity}
+          onValueChange={handlePasswordChange}
+        />
+        <button className="form__button button button_place_screen" type="submit" disabled={!formValidity}>{buttonText}</button>
+      </form>
+      {(props.form === 'register') && (<a className="container__sign-in" href="/sign-in">Уже зарегистрировались? Войти</a>)}
+    </>
   );
 }
 
