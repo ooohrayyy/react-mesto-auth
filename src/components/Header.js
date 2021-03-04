d6ea565c3c449d911fd5f71073bae9e47e2640eb
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-04 12:04:57 +0300

    feat: Написать логику для header__register

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import logo from '../resources/images/svgs/logo.svg';
 
-function Header () {
+function Header (props) {
 
   // * Стейт-переменные
 
@@ -16,9 +16,22 @@ function Header () {
 
   // * Переменные
 
-  const menuButtonStyle = menuIsOpen ? 'header__menu-btn_open' : 'header__menu-btn_closed'; // Класс кнопки меню
+  const email = 'email@mail.com'; // E-mail пользователя
 
-  let email = 'email@mail.com'; // E-mail пользователя
+  let link = {}; // Свойства ссылки
+
+  if (props.place === 'main') {
+    link.url = './sign-in';
+    link.text = 'Выйти';
+  } else if (props.place === 'register') {
+    link.url = './sign-in';
+    link.text = 'Войти';
+  } else if (props.place === 'login') {
+    link.url = './sing-up';
+    link.text = 'Регистрация';
+  }
+
+  const menuButtonStyle = menuIsOpen ? 'header__menu-btn_open' : 'header__menu-btn_closed'; // Класс кнопки меню
 
   // * Возвращаемое значение
 
@@ -26,12 +39,12 @@ function Header () {
     <header className="container__header header">
       <menu className="header__menu">
         <p className="header__email">{email}</p>
-        <a className="header__register" src={linkUrl}>{linkText}</a>
+        <a className={`header__register ${(props.place === 'main') && 'header__register_main'}`} src={link.url}>{link.text}</a>
       </menu>
       <div className="header__body">
         <img className="header__logo" src={logo} alt="Логотип Mesto" />
         <p className="header__email">{email}</p>
-        <a className="header__register" src={linkUrl}>{linkText}</a>
+        <a className="header__register" src={link.url}>{link.text}</a>
         <button className={menuButtonStyle} onClick={toggleMenu} />
       </div>
     </header>
