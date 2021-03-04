187cc43e30170ea46e27840cc4f362fadaf81e56
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-04 10:57:27 +0300

    feat: Добавить логику для кнопки хэдера

--- src/components/Header2.js
+++ src/components/Header2.js
@@ -1,17 +1,28 @@
+import React from 'react';
+
 import logo from '../resources/images/svgs/logo.svg';
 
-function Header (props) {
+function Header () {
+  const [menuIsOpen, setMenuIsOpen] = React.useState(false);
+
+  function toggleMenu () {
+    setMenuIsOpen(!menuIsOpen);
+  }
+
+  const menuButtonStyle = menuIsOpen ? 'header__menu-btn_open' : 'header__menu-btn_closed';
+
   return (
-    <header хэдер>
-      <menu хэдер__меню>
+    <header className="container__header header">
+      <menu className="header__menu">
         Email
         Ссылка
       </menu>
-      <div хэдер__бади>
-        <img хэдер__лого />
+      <div className="header__body">
+        <img className="header__logo" src={logo} alt="Логотип Mesto" />
         Email
         Ссылка
         Кнопка
+        <button className={menuButtonStyle} onClick={toggleMenu} />
       </div>
     </header>
   );
