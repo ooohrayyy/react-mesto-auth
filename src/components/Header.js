ea82ca79a4b24406b9a2f0a8e40a7e63c70ca7f5
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-04 12:05:38 +0300

    chore: Удалить лишние файлы и комментарии

--- src/components/Header.js
+++ src/components/Header.js
@@ -52,24 +52,3 @@ function Header (props) {
 }
 
 export default Header;
-
-// import logo from '../resources/images/svgs/logo.svg';
-
-// function Header (props) {
-//   // * Переменные
-
-//   const registerLink = (<a className="header__register" href="/sign-up">Регистрация</a>);
-//   const authorizeLink = (<a className="header__register" href="/sign-in">Войти</a>);
-
-//   // * Возвращаемое значение
-
-//   return (
-//     <header className="container__header header">
-//       <img className="header__logo" src={logo} alt="Логотип Mesto" />
-//       {(props.place === 'login') && registerLink}
-//       {(props.place === 'register') && authorizeLink}
-//     </header>
-//   );
-// }
-
-// export default Header;
