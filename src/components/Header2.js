5ee54f9d3ba68e8ba38e7dffa20f0dd76989263f
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-04 10:51:45 +0300

    feat: Написать псевдокод для хэдера

---
+++ src/components/Header2.js
@@ -0,0 +1,20 @@
+import logo from '../resources/images/svgs/logo.svg';
+
+function Header (props) {
+  return (
+    <header хэдер>
+      <menu хэдер__меню>
+        Email
+        Ссылка
+      </menu>
+      <div хэдер__бади>
+        <img хэдер__лого />
+        Email
+        Ссылка
+        Кнопка
+      </div>
+    </header>
+  );
+}
+
+export default Header;
