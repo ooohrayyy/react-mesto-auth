ae103797a6207ee483ec7c0a29c371a7d880a776
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-04 13:00:16 +0300

    feat: Добавить блок header__out

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,10 +20,7 @@ function Header (props) {
 
   let link = {}; // Свойства ссылки
 
-  if (props.place === 'main') {
-    link.url = './sign-in';
-    link.text = 'Выйти';
-  } else if (props.place === 'register') {
+  if (props.place === 'register') {
     link.url = './sign-in';
     link.text = 'Войти';
   } else if (props.place === 'login') {
@@ -39,13 +36,14 @@ function Header (props) {
     <header className="container__header header">
       {((props.place === 'main') && menuIsOpen) && (<menu className="header__menu">
         <p className="header__email">{email}</p>
-        <a className={`header__link ${(props.place === 'main') && 'header__link_out'}`} href={link.url}>{link.text}</a>
+        <button className="header__out">Выйти</button>
       </menu>)}
       <div className="header__body">
         <img className="header__logo" src={logo} alt="Логотип Mesto" />
         {(props.place === 'main') && (<p className="header__email header__email_place_body">{email}</p>)}
-        <a className={`header__link ${(props.place === 'main') && 'header__link_out'}`} href={link.url}>{link.text}</a>
+        {(props.place === 'main') && (<button className="header__out">Выйти</button>)}
         {(props.place === 'main') && (<button className={menuButtonStyle} onClick={toggleMenu} />)}
+        {(props.place !== 'main') && (<a className="header__link" href={link.url}>{link.text}</a>)}
       </div>
     </header>
   );
