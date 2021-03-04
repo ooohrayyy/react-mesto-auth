726aa85aa02f9605705e800094121cf8fd15fa66
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-04 11:20:39 +0300

    feat: Переписать стили для хэдера

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,52 @@
+import React from 'react';
+
 import logo from '../resources/images/svgs/logo.svg';
 
-function Header (props) {
-  // * Переменные
+function Header () {
+  const [menuIsOpen, setMenuIsOpen] = React.useState(false);
 
-  const registerLink = (<a className="header__register" href="/sign-up">Регистрация</a>);
-  const authorizeLink = (<a className="header__register" href="/sign-in">Войти</a>);
+  function toggleMenu () {
+    setMenuIsOpen(!menuIsOpen);
+  }
 
-  // * Возвращаемое значение
+  const menuButtonStyle = menuIsOpen ? 'header__menu-btn_open' : 'header__menu-btn_closed';
 
   return (
     <header className="container__header header">
-      <img className="header__logo" src={logo} alt="Логотип Mesto" />
-      {(props.place === 'login') && registerLink}
-      {(props.place === 'register') && authorizeLink}
+      <menu className="header__menu">
+        Email
+        Ссылка
+      </menu>
+      <div className="header__body">
+        <img className="header__logo" src={logo} alt="Логотип Mesto" />
+        Email
+        Ссылка
+        Кнопка
+        <button className={menuButtonStyle} onClick={toggleMenu} />
+      </div>
     </header>
   );
 }
 
 export default Header;
+
+// import logo from '../resources/images/svgs/logo.svg';
+
+// function Header (props) {
+//   // * Переменные
+
+//   const registerLink = (<a className="header__register" href="/sign-up">Регистрация</a>);
+//   const authorizeLink = (<a className="header__register" href="/sign-in">Войти</a>);
+
+//   // * Возвращаемое значение
+
+//   return (
+//     <header className="container__header header">
+//       <img className="header__logo" src={logo} alt="Логотип Mesto" />
+//       {(props.place === 'login') && registerLink}
+//       {(props.place === 'register') && authorizeLink}
+//     </header>
+//   );
+// }
+
+// export default Header;
